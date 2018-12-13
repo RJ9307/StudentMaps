065c042b0eb55fb57e6502a172faf11d12f5798f
NickP <nick.pihay@student.ap.be>
2018-12-13 22:26:36 +0100

    3 werkende checkboxen + gekleurde pinnen (voor te testen)

---
+++ Website_vNick/index.js
@@ -0,0 +1,49 @@
+/*
+ * npm init
+ * npm install express --save
+ * npm install request --save
+ * npm install ejs --save
+ *
+ * node(mon) index.js
+ */
+
+var express = require('express');
+var request = require('request');
+var path = require('path');
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+var cafeData;
+request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek/MapServer/28/query?where=1%3D1&outFields=OBJECTID,ROEPNAAM_ZZ_OBJECT,NAAM_ZAAL&outSR=4326&f=json',
+  function(error, response, body){
+    cafeData = JSON.parse(body);
+  });
+
+  var cultuurData;
+  request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek/MapServer/292/query?where=1%3D1&outFields=OBJECTID,locatie_adres_ID,categorie,naam,straat,huisnr,busnr,link,toegankelijk,X,Y,LAT,LON,postcode,gemeente&outSR=4326&f=json',
+    function(error, response, body){
+      cultuurData = JSON.parse(body);
+    });
+
+var sportData;
+  request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek/MapServer/643/query?where=1%3D1&outFields=*&outSR=4326&f=json',
+    function(error, response, body){
+      sportData = JSON.parse(body);
+    });
+
+app.get('/', function(req, res){
+  res.render('home',{
+    fuifruimte: cafeData,
+    cultuurruimte: cultuurData,
+    sportruimte: sportData
+  });
+});
+
+
+
+app.listen(3000);
